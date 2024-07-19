0b7375e3b32000f5a2034c8b3aca2c5839f7c83b
NasserSami <salamat247@gmail.com>
2024-07-19 15:59:04 -0400

    Initial Commit

---
+++ Pandemic Modeler/src/Main.java
@@ -0,0 +1,18 @@
+/**
+ * Program Name: Main.java
+ * Purpose: 
+ * Coders: 
+ * Date: Jul 19, 2024
+ */
+
+public class Main
+{
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+	}
+	//end main
+}
+//end class
